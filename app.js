df5c3cce6bd96a1a05ee762ca0e70774d15603d1
Akshay72023 <al7864745@gmail.com>
2023-10-26 07:55:53 +0530

    data is parsed

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const express= require('express');
+app.use(bodyParser.urlencoded({extended:false}));
 
-const app=express();
+app.use('/add-product',(req, res, next) => {
+  res.send('<html><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form></html>');
+});
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
-})
+app.post('/product',(req,res,next)=>{
+        console.log(req.body);
+        res.redirect('/')
+});
 
-app.use((req,res,next)=>{
-    console.log('In another middleware')
-    res.send('<h1>Hello from express</h1>')
-})
-// const server = http.createServer(app);
-// server.listen(3001);
+app.use('/',(req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-app.listen(3001);
+app.listen(3000);
