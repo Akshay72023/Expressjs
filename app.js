8617844a69f95ce708483d58b08576d1b8422027
Akshay72023 <al7864745@gmail.com>
2023-10-27 08:22:19 +0530

    Contact us is added

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ app.use(express.static(path.join(__dirname,'public')));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes=require('./routes/contact')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
 app.use('/shop',shopRoutes);
+app.use(contactRoutes);
+
+app.use('/success',(req,res,next)=>{
+      res.sendFile(path.join(__dirname,'Views','success.html'))
+});
 
 app.use('/',(req,res,next) => {
       res.status(404).sendFile(path.join(__dirname,'Views','404.html'));
 });
+
+
 
 app.listen(3000);
